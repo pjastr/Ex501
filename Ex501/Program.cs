edb75d317b4e0c73b3d01e350f1714e39f2bf534
Piotrek <@>
2017-11-02 12:54:13 +0100

    przesłanie plików

---
+++ Ex501/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex501
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Temperatura> temperatury = new List<Temperatura>();
+            temperatury.Add(new Temperatura(45.2));
+            temperatury.Add(new Temperatura(-12.3));
+            temperatury.Add(new Temperatura(0));
+            temperatury.Sort();
+        }
+    }
+}
---
+++ Ex501/Temperatura.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex501
+{
+    class Temperatura:IComparable<Temperatura>
+    {
+        private double wartosc;
+
+        public Temperatura(double wart)
+        {
+            this.wartosc = wart;
+        }
+
+        public int CompareTo(Temperatura other)
+        {
+            if (other.wartosc > this.wartosc)
+            {
+                return 1;
+            }
+            else if (other.wartosc == this.wartosc)
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
